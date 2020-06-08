2e13b64c9e52e5a90f92db6ebbbb4e4d1e70ad5c
sieun <se0983@kookmin.ac.kr>
2020-06-08 19:27:30 +0900

    (6주차과제) bookmark

---
+++ main/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Bookmark
+
+class BookmarkForm(forms.ModelForm):
+    site_name = forms.CharField(label="사이트 명")
+    site_url = forms.CharField(label="사이트 주소")
+
+    class Meta:
+        model = Bookmark
+        fields = '__all__' #Bookmark필드 다 가져와
+
